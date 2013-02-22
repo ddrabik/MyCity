ef119e505da020aeef5ac176fe77819808028d68
David Drabik <djdrabik@gmail.com>
2013-02-21 19:52:36 -0800

    added sending capability to chat service

--- src/com/cs110/mycity/ChatService.java
+++ src/com/cs110/mycity/ChatService.java
@@ -14,46 +14,66 @@ import android.util.Log;
 
 public class ChatService extends Service {
 	private static final String TAG = "CHATSERVICE";
-	
+
 	private ChatManager chatmanager = null;
-	
+	private XMPPConnection connection;
+
+	private static ChatService mInstance;
+
+	public synchronized static ChatService getInstance() {
+		if(mInstance==null){
+			mInstance = new ChatService();
+		}
+		return mInstance;
+	}
+
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
 	public void onCreate() {
-		Log.d(TAG, "Chatservice called");
-		XMPPConnection connection = XMPPLogic.getInstance().getConnection();
+		mInstance = ChatService.this;
+		connection = XMPPLogic.getInstance().getConnection();
 		chatmanager = connection.getChatManager();
 		chatmanager.addChatListener(
-			    new ChatManagerListener() {
-			        @Override
-			        public void chatCreated(Chat chat, boolean createdLocally)
-			        {
-			            if (!createdLocally)
-			                chat.addMessageListener(new MessageListener() {
-			                    @Override
+				new ChatManagerListener() {
+					@Override
+					public void chatCreated(Chat chat, boolean createdLocally)
+					{
+						if (!createdLocally)
+							chat.addMessageListener(new MessageListener() {
+								@Override
 								public void processMessage(Chat chat, Message message) {
-			                    	if(message.getBody() != null) {
-				                    	ChatHelper chatHelper = ChatHelper.getInstance();
-				                    	String from = message.getFrom();
-				                    	from = from.substring(0, from.indexOf('/'));
-				                    	chatHelper.newMessageReceived(from, message.getBody());				                    	
-			                    	}
-			                    }
-			                });
-			        }
-			    });
+									if(message.getBody() != null) {
+										ChatHelper chatHelper = ChatHelper.getInstance();
+										String from = message.getFrom();
+										from = from.substring(0, from.indexOf('/'));
+										Log.d(TAG, "Received from " + from + ": " + message.getBody());	
+										chatHelper.newMessageReceived(from, message.getBody());				                    	
+									}
+								}
+							});
+					}
+				});
 	}
 	
+	public void sendMessageTo(String buddy, String text) {
+		Message msg = new Message(buddy, Message.Type.chat); 
+		msg.setBody(text);
+		Log.d(TAG, "Sending to " + buddy + ": " + text);	
+		if(connection != null) {
+			connection.sendPacket(msg);
+		}
+	}
+
 	@Override
 	public void onDestroy() {
 		Log.d(TAG, "Destroying chat service");
 	}
-	
+
 	@Override
 	public void onStart(Intent intent, int startid) {
 		Log.d(TAG, "Starting chat service");
